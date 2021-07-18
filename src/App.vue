7763c306cc1cc6945158916cb326eb1b4e547bc4
unknown <you@example.com>
2021-07-18 16:39:05 +0900

    vuex_basic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="home">
+    <div id="nav"></div>
+    <div class="counter">{{ $store.state.counter }}</div>
+    <div class="buttons">
+      <!-- mutation의 함수이용은  $store.commit('decrease')"-->
+      <!-- action(비동기 등)은 지금처럼 사용-->
+      <button @click="$store.dispatch('decrease')">-</button>
+      <button @click="$store.dispatch('increase')">+</button>
+    </div>
+    <!-- v-model로 인풋상태값 저장 -->
+    <input v-model="inputTest" type="text" placeholder="dataKip" />
   </div>
-  <router-view/>
 </template>
+<script>
+export default {
+  name: "home",
+
+  data() {
+    return {
+      counter: 0,
+      inputTest: "hihi",
+    };
+  },
+
+  methods: {},
+};
+</script>
 
 <style>
 #app {
@@ -13,6 +34,19 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+div {
+  margin-bottom: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
 }
 
 #nav {
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,38 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    inputData: 'hello',
   },
   mutations: {
+    decrease(state, randomNumber) {
+      state.counter -= parseInt(randomNumber[0])
+    },
+    increase(state, randomNumber) {
+
+      state.counter += parseInt(randomNumber[0])
+
+    },
   },
   actions: {
+    increase({ commit }) {
+      axios('https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new').then((res) => {
+        commit('increase', res.data)
+
+      })
+    },
+    decrease({ commit }) {
+      axios('https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new').then((res) => {
+        commit('increase', res.data)
+
+      })
+    }
+
+  },
+  getters: {
+
   },
   modules: {
   }
